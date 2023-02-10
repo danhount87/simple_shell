4fba39d8d47445d63164f244b12144661e0492f1
Nice22 <benothouessou74@gmail.com>
2023-02-10 15:15:31 +0100

    ok

--- memory.c
+++ memory.c
@@ -12,19 +12,18 @@
 
 /* Nice22 @zinzinpolice */
 /**
- * bfree - frees a pointer and NULLs the address
+ * bfree - frees a pointer and NULLs the address
  * @ptr: address of the pointer to free
- *
- * Return: 1 if freed, otherwise 0.
- */
+ * Return: 1 if freed, otherwise 0
+ */
 int bfree(void **ptr)
 {
- if (ptr && *ptr)
- {
-  free(*ptr);
-  *ptr = NULL;
-  return (1);
- }
- return (0);
- }
+    if (ptr && *ptr)
+    {
+        free(*ptr);
+        *ptr = NULL;
+        return (1);
+    }
+    return (0);
+}
 
